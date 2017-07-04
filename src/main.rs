30f5a1551d166bf9bd0127369fc5337bf1df7da1
michele <michele.damico@gmail.com>
2017-07-04 20:22:40 +0200

    I: parse int strings

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ fn main() {
     println!("Hello, string_adder!");
 }
 
-pub fn add(_data: &str) -> i32 {
-    0
+pub fn add(data: &str) -> i32 {
+    if data.is_empty() {
+        return 0
+    }
+    data.parse().unwrap()
 }
 
 #[cfg(test)]
@@ -13,6 +16,12 @@ mod tests {
     #[test]
     fn empty_string_should_return_0() {
         assert_eq!(0, add(""));
+    }
+
+    #[test]
+    fn should_return_parsed_integers() {
+        assert_eq!(3, add("3"));
+        assert_eq!(12, add("12"));
     }
 
 }
