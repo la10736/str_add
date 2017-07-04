710cbe1936cdae6bbda72ebc53a138b4b0927541
michele <michele.damico@gmail.com>
2017-07-04 21:12:40 +0200

    I: parse header

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ pub fn add(data: &str) -> Values {
     if data.is_empty() {
         return 0
     }
+    if data.starts_with("//["){
+        let separator = data.chars().nth(3).unwrap();
+        let itemizer = Itemize::new(separator);
+
+        return itemizer.itemize(data.splitn(2,'\n').nth(1).unwrap()).sum()
+    }
     Itemize::default().itemize(data).sum()
 }
 
@@ -66,5 +72,11 @@ mod tests {
     fn also_carriage_return_is_a_separator() {
         assert_eq!(3, add("1\n2"));
         assert_eq!(25, add("1,2\n3,4,12\n1\n2"));
+    }
+
+    #[test]
+    fn should_parse_header_to_seect_correct_separator() {
+        assert_eq!(3, add("//[;]\n1;2"));
+        assert_eq!(17, add("//[;]\n1;2\n12;2"));
     }
 }
