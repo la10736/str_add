329e1937678ed1dfc7f44f2032877e76a675202b
michele <michele.damico@gmail.com>
2017-07-04 20:19:08 +0200

    I: empty string return 0

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@ fn main() {
     println!("Hello, string_adder!");
 }
 
+pub fn add(_data: &str) -> i32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn empty_string_should_return_0() {
+        assert_eq!(0, add(""));
+    }
 
 }
